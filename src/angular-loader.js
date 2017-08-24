7fd6de9890334b873370fc06160dfcc8ca450970
Adil Sulaiman <asulaimanificent@gmail.com>
2017-08-24 10:21:47 -0400

    Added initial loader source code.

---
+++ src/angular-loader.js
@@ -0,0 +1,44 @@
+let updateBindings = [];
+/**
+ * Returns array of objects in module
+ * @param {*} mod 
+ */
+function parseModule(mod) {
+    return mod.match(/\{([\S\s]*)\}/).map((item) => {
+        let obj;
+        try {
+        obj = eval('(' + item.trim() + ')');
+        }
+        finally {
+            return obj;
+        }
+    });
+}
+
+/**
+ * Adds list of bindings to update for components
+ * @param {*} objects 
+ */
+function parseComponents(objects) {
+    objects.forEach(function(obj) {
+        //look for component signature
+        if (obj && obj.bindings) {
+            for (const i in obj.bindings) {
+                if (obj.bindings[i].indexOf('::') > -1) {
+                    updateBindings.push({name: obj.name, binding: i})
+                }
+            }
+        }
+    }, this);
+}
+
+function stripModule(mod) {
+    const supportedBindings = ['::=', '::<', '::@'];
+    supportedBindings.forEach((binding) => mod.replace(binding, binding.substr(2)));
+    return mod;
+}
+module.exports = function(content) {
+    parseComponents(parseModule(content));
+    const strippedModule = stripModule(content);
+    return content;
+}
