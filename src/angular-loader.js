dfa4d44226a9385b3ac16ad36f9a12ecb6b69413
Adil Sulaiman <ASulaiman@ifdsgroup.com>
2017-08-28 16:19:07 -0400

    Setup one-time binding parsing and persisted state.

--- src/angular-loader.js
+++ src/angular-loader.js
@@ -1,43 +1,55 @@
-let updateBindings = [];
+const esprima = require('esprima');
+const get = require('lodash/get');
+
+function isExportSupported(mod) {
+    return (mod.type === 'ExportNamedDeclaration' || mod.type === 'ExportDefaultDeclaration') && mod.declaration.type === 'ObjectExpression';
+}
+
+function getOneTimeBindings(bindings) {
+    const supportedBindings = ['::=', '::=?', '::<', '::<?', '::@', '::@?'];
+    return bindings.reduce((oneTimeBindings, binding) => {
+        if (supportedBindings.includes(binding.value.value)) {
+            oneTimeBindings.push(binding.key.name);
+            return oneTimeBindings;
+        }
+        return oneTimeBindings;
+    }, []);
+}
+
+function getComponentToUpdate(properties) {
+    return properties.reduce((props, property) => {
+        if (property.key.name === 'name'){
+            props.name = property.value.value;
+        } 
+        else if (property.key.name === 'bindings') {
+            props.bindings = getOneTimeBindings(property.value.properties);
+            
+        };
+        return props;
+    }, {});
+
+}
+
 /**
  * Returns array of objects in module
  * @param {*} mod 
  */
 function parseModule(mod) {
-    return mod.match(/\{([\S\s]*)\}/).map((item) => {
-        let obj;
-        try {
-        obj = eval('(' + item.trim() + ')');
-        }
-        finally {
-            return obj;
-        }
-    });
-}
-
-/**
- * Adds list of bindings to update for components
- * @param {*} objects 
- */
-function parseComponents(objects) {
-    objects.forEach(function(obj) {
-        //look for component signature
-        if (obj && obj.bindings) {
-            for (const i in obj.bindings) {
-                if (obj.bindings[i].indexOf('::') > -1) {
-                    updateBindings.push({name: obj.name, binding: i})
-                }
+    const parsedMod = esprima.parseModule(mod, { tolerant: true });
+    return parsedMod.body.reduce((componentList, modItem) => {
+        if (isExportSupported(modItem)) {
+            const component = getComponentToUpdate(modItem.declaration.properties);
+            if (component && component.name && component.bindings && component.bindings.length > 0) {
+                componentList.push(component);
             }
         }
-    }, this);
+        return componentList;
+    },[]);
+    
 }
 
-function stripModule(mod) {
-    const supportedBindings = ['::=', '::<', '::@'];
-    return supportedBindings.reduce((item, binding) => item.replace(binding, binding.substr(2)), mod);
-}
 module.exports = function(content) {
-    parseComponents(parseModule(content));
-    const strippedModule = stripModule(content);
+    module.updateBindings = module.updateBindings || [];
+    module.updateBindings.push(...parseModule(content));
     return content;
 }
